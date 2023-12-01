78bb672b8ab6ba24f2edfaebcb967343a653fe2b
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-12-01 01:00:48 +0000

    Add product details view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Product
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products, includes sorting and searches """
 
     products = Product.objects.all()
 
@@ -10,3 +10,15 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_view(request, product_id):
+    """ View to see full product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_view.html', context)
