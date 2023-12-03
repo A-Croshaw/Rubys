7168a848ff229ff5e5afb62c58f895edcaba04a4
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-12-03 01:30:15 +0000

    added model forms and admin to checkout

--- rubys/settings.py
+++ rubys/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_countries', 
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
