b50d2b3db20a6667f6b93528223453de7a1d5ed1
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-12-04 19:35:24 +0000

    update database

--- rubys/settings.py
+++ rubys/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://rubysbooksonline-7c229ec277f8.herokuapp.com/', 'localhost','127.0.0.1']
 
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django.contrib.sites',
     'django_countries', 
     'allauth',
